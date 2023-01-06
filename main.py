f51d30c5b0cf0b9df74eefb36450fff36afb90a2
Kassio Vieira <kassio1206@hotmail.com>
2023-01-05 22:44:49 -0300

    teste

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+https://medium.com/data-hackers/t%C3%A9cnicas-de-processamento-digital-de-imagens-no-python-com-matem%C3%A1tica-154eae204447
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt 
 from skimage import data
