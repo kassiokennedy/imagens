12984031a7e704a181cd470e32b019eb442b18d1
Kassio Vieira <kassio1206@hotmail.com>
2023-01-12 15:53:48 -0300

    teste

---
+++ projetoImagem.py
@@ -0,0 +1,12 @@
+'''
+https://academiapme-my.sharepoint.com/:w:/g/personal/renato_dio_me/EUul9wFynQ9Mqg2Ah2BaKNsBEOX9ZRa4DGQjuvMkz-4NfA?rtime=eUVzj4342kg
+https://web.dio.me/lab/projeto-de-sensores-inteligentes-para-iot/learning/bdc556e6-c090-4d00-8050-6b161b6a6bea
+
+Projeto Final – Sensores Inteligentes para IoT
+
+O projeto para Sensores Inteligentes envolve uma aplicação para imagens digitais, onde algum tipo de objeto deve ser utilizado como foco na tarefa de detecção. A rede utilizada deve ser o modelo YOLO, um tipo de rede de Deep Learning capaz de avaliar imagens e detectar e classificar objetos específicos, treinados previamente na base de dados. O modelo está disponível no link: (https://colab.research.google.com/github/aissam-out/YOLO/blob/master/YOLO_ImageAI_video.ipynb).
+
+Transfer Learning: https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/images/transfer_learning.ipynb
+
+A tarefa pode envolver a detecção de pragas em plantações, pessoas nas ruas, câncer de pele, frutos maduros em uma plantação, desmatamento, dentre outras situações.
+'''
